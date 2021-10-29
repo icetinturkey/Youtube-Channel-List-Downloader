3522e742191c712e128d1d359c25c4e991ac7515
icetinturkey <41130134+icetinturkey@users.noreply.github.com>
2021-10-29 13:13:10 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
 <html>
 <body>
-<h1>Merhaba Dünya</h1>
-<p>I'm hosted with GitHub Pages.</p>
-<p><i>Guild Anatolia Panel Project</i></p>
-<?php echo("Php Confirmed !"); ?>
+<h1>Guild Anatolia Panel Project</h1>
+<div id="loadedDiv"></div>
+
 </body>
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      if (window.XMLHttpRequest) {xmlhttp=new XMLHttpRequest();}
+	else {xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");}
+	xmlhttp.onreadystatechange=function() {
+	if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+    document.getElementById("loadedDiv").innerHTML = xmlhttp.responseText;
+    }
+	}
+	xmlhttp.open("GET","http://api.pathofexile.com/leagues?type=main&compact=1",true);
+	xmlhttp.send();
+      });
+  </script>
 </html>
